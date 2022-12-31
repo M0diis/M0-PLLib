e5dba760d908987e0b27fde956fb31d6671c004f
M0diis <modestas.kazlauskas@ktu.edu>
2023-01-01 01:18:42 +0200

    Collection Utils

---
+++ src/main/java/me/m0dii/pllib/utils/CollectionUtils.java
@@ -0,0 +1,48 @@
+package me.m0dii.pllib.utils;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CollectionUtils {
+    public static List<String> addStarting(List<String> list, String toMatch, String... toAdd) {
+        if(list == null || toMatch == null || toAdd == null) {
+            return list;
+        }
+
+        Stream.of(toAdd).filter(s -> StringUtils.startsWithIgnoreCase(s, toMatch)).forEach(list::add);
+
+        return list;
+    }
+
+    public static List<String> addEnding(List<String> list, String toMatch, String... toAdd) {
+        if(list == null || toMatch == null || toAdd == null) {
+            return list;
+        }
+
+        Stream.of(toAdd).filter(s -> StringUtils.endsWithIgnoreCase(s, toMatch)).forEach(list::add);
+
+        return list;
+    }
+
+    public static List<String> addMatching(List<String> list, String toMatch, String... toAdd) {
+        if(list == null || toMatch == null || toAdd == null) {
+            return list;
+        }
+
+        Stream.of(toAdd).filter(s -> s.equalsIgnoreCase(toMatch)).forEach(list::add);
+
+        return list;
+    }
+
+    public static List<String> addContaining(List<String> list, String toMatch, String... toAdd) {
+        if(list == null || toMatch == null || toAdd == null) {
+            return list;
+        }
+
+        Stream.of(toAdd).filter(s -> StringUtils.containsIgnoreCase(s, toMatch)).forEach(list::add);
+
+        return list;
+    }
+}
