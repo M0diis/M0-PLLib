d62cccd135249a9b6f26ebf1c794c6989dc8f18b
M0diis <modestas.kazlauskas@ktu.edu>
2023-01-04 11:24:19 +0200

    Update roman conversion

--- src/main/java/me/m0dii/pllib/utils/Utils.java
+++ src/main/java/me/m0dii/pllib/utils/Utils.java
@@ -17,12 +17,22 @@ public class Utils {
             case 8 -> "VIII";
             case 9 -> "IX";
             case 10 -> "X";
-            default -> String.valueOf(level);
+            case 11 -> "XI";
+            case 12 -> "XII";
+            case 13 -> "XIII";
+            case 14 -> "XIV";
+            case 15 -> "XV";
+            case 16 -> "XVI";
+            case 17 -> "XVII";
+            case 18 -> "XVIII";
+            case 19 -> "XIX";
+            case 20 -> "XX";
+            default -> "XX+";
         };
     }
 
     public static String romanToArabic(String roman) {
-        return switch (roman) {
+        return switch (roman.toUpperCase()) {
             case "I" -> "1";
             case "II" -> "2";
             case "III" -> "3";
@@ -33,6 +43,16 @@ public class Utils {
             case "VIII" -> "8";
             case "IX" -> "9";
             case "X" -> "10";
+            case "XI" -> "11";
+            case "XII" -> "12";
+            case "XIII" -> "13";
+            case "XIV" -> "14";
+            case "XV" -> "15";
+            case "XVI" -> "16";
+            case "XVII" -> "17";
+            case "XVIII" -> "18";
+            case "XIX" -> "19";
+            case "XX" -> "20";
             default -> roman;
         };
     }
